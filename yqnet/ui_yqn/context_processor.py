3bbab1a571c7f7ba89e678f174f37bbadb1b526c
Michael Wood <internet@michaelwood.me.uk>
2019-05-09 20:58:15 +0100

    context_processor: Add Quakr App flag for future use

--- yqnet/ui_yqn/context_processor.py
+++ yqnet/ui_yqn/context_processor.py
@@ -3,6 +3,8 @@
 import settings_yqn.settings
 
 def common(request):
+
     return {
         "DEBUG" : settings_yqn.settings.DEBUG,
+        "QUAKRAPP" : "quakrapp" in request.META['HTTP_USER_AGENT'],
     }
